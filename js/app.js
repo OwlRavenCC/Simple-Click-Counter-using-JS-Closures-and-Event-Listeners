1e4b58008557d26d0e37e02f30101f973e0fd146
Carlos Castaneda <raven@owlraven.com>
2018-06-24 13:13:24 -0700

    version 1.0 KnockoutJs

--- js/app.js
+++ js/app.js
@@ -26,133 +26,20 @@ var catModel = function(){
   ]);
 
   self.currentCat = ko.observable(self.cats()[0]);
+  self.currentCount = ko.observable(self.currentCat().clickCount);
+
 
   self.showCat = function(indexValue){
     self.currentCat(self.cats()[indexValue]);
+    self.incrementCounter();
   };
 
-
+  self.incrementCounter = function(){
+    var cat = self.currentCat();
+    cat.clickCount++;
+    var count = cat.clickCount;
+    self.currentCount(count);
+  };
 };
 
 ko.applyBindings(new catModel());
-
-// /* ======= View ====== */
-//
-// var ViewDisplay = {
-//   init: function(){
-//     this.name = $('.display-name');
-//     this.image = $('.image-cat');
-//     this.count = $('.display-count');
-//
-//     this.image.on('click', function() {
-//       controller.incrementCounter();
-//     });
-//
-//     this.render();
-//   },
-//
-//   render: function(){
-//     var currentCat = controller.getCurrentCat();
-//     this.name.text(currentCat.name);
-//     this.image.attr('src', currentCat.image) ;
-//     this.count.text(currentCat.clickCount);
-//   }
-//
-// };
-
-//
-// var viewList = {
-//   init: function(){
-//     this.catlist = $('#catslist');
-//     this.render();
-//   },
-//
-//   render: function(){
-//     var cats = controller.getCats();
-//     this.catlist.empty();
-//
-//
-//     for (var i = 0; i < cats.length; i++){
-//       var cat = cats[i];
-//
-//       var elem = document.createElement("li");
-//       $(elem).addClass('list-group-item');
-//
-//       elem.textContent = cat.name;
-// //Here I used a IFFE (Immediately-invoked function expression ) to avoid replacing the counter at the end of the loop.
-//       $(elem).on('click', (function(catCopy) {
-//         return function() {
-//           controller.setCurrentCat(catCopy);
-//           controller.incrementCounter();
-//         };
-//       })(cat));
-//       this.catlist.append(elem);
-//
-//
-//
-//     };
-//
-//   }
-// };
-//
-//
-// var adminPanel = {
-//   init: function(){
-//     this.activator = $('#admin-button');
-//     this.panel = $('#admin-control');
-//     this.panel.toggle(0);
-//
-//     this.render();
-//   },
-//
-//   render: function(){
-//     var panel = this.panel;
-//
-//     this.activator.on('click', function(){
-//       panel.toggle('slow');
-//     });
-//   }
-// };
-//
-//
-// var adminInput = {
-//   init: function(){
-//
-//     this.nameadmin = $('#admin-cat-name');
-//     this.imageadmin = $('#admin-cat-image');
-//     this.numberadmin = $('#admin-cat-number');
-//
-//
-//     this.submitBtn =  $('#admin-submit');
-//     this.cancelBtn =  $('#admin-cancel');
-//
-//     this.cancelBtn.on('click',function(){
-//       adminPanel.panel.toggle('slow');
-//     });
-//     this.render();
-//   },
-//
-//   render: function(){
-//     var currentCat = null;
-//     var currentCat = controller.getCurrentCat();
-//
-//      this.nameadmin.val(currentCat.name);
-//      this.imageadmin.val(currentCat.image);
-//      this.numberadmin.val(currentCat.clickCount);
-//
-//      this.submitBtn.on('click',function(){
-//        var currentCat = null;
-//        var currentCat = controller.getCurrentCat();
-//
-//       currentCat.name = adminInput.nameadmin.val();
-//       currentCat.image = adminInput.imageadmin.val();
-//       currentCat.clickCount= adminInput.numberadmin.val();
-//       viewList.render();
-//       ViewDisplay.render();
-//       adminPanel.panel.hide();
-//     });
-//   }
-//
-// };
-//
-// controller.init();
